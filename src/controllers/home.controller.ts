c7fa5279e753b9ba7fa8ddbe1362c5614df22efb
Vu Tuan Anh <vtanh1905@gmail.com>
2022-07-21 23:33:18 +0700

    remove useless code

--- src/controllers/home.controller.ts
+++ src/controllers/home.controller.ts
@@ -2,25 +2,14 @@ import { controller, httpGet } from "inversify-express-utils";
 import { Request, Response } from "express";
 
 import { IHomeController } from "./interfaces/ihome.controller";
-import { inject } from "inversify";
-
-import { S3Service } from "../utils";
-
-import { env } from "../constants";
 
 @controller("")
 export class HomeController implements IHomeController {
-  constructor(@inject("S3Service") private s3Service: S3Service) {}
+  constructor() {}
 
   @httpGet("/")
   public async get(req: Request, res: Response): Promise<number> {
     res.sendFile("./web/dist/index.html");
-    return 1;
-  }
-
-  @httpGet("/test")
-  public async upload(req: Request, res: Response): Promise<number> {
-    res.send(env);
     return 1;
   }
 }
